b429ea4d83c62dae9e01315ea6b73f5247773240
khurshiddev <umaribnxattob8008@gmail.com>
2022-11-20 23:00:03 +0900

    [add::] looping_and_ranges

---
+++ src/main/kotlin/looping_and_ranges/looping_and_ranges.5.kt
@@ -0,0 +1,17 @@
+package looping_and_ranges
+
+fun main() {
+    for (i in 1..30)
+        print("$i ")
+    println()
+    for (i in 30 downTo 1)
+        print("$i ")
+    println()
+    for (i in 1.rangeTo(30))
+        print("$i ")
+    println()
+    for (i in 30.downTo(1))
+        print("$i ")
+    println()
+
+}
