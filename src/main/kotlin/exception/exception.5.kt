0e32e3c5f1574bfc5fc870c633c3a9c3266ecc52
khurshiddev <umaribnxattob8008@gmail.com>
2022-11-23 13:03:09 +0900

    [add::] exception.5.kt

---
+++ src/main/kotlin/exception/exception.5.kt
@@ -0,0 +1,12 @@
+package exception
+
+fun main(){
+    try{
+        var ar = arrayOf(1,2,3,4,5)
+        var int = ar[6]
+        println(int)
+    }
+    finally {
+        println("This block always executes")
+    }
+}
