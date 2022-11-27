dc2fb8ec8a94d0fc0a084cb53abbc045cac82d28
khurshiddev <umaribnxattob8008@gmail.com>
2022-11-27 23:14:14 +0900

    [add::] map_1.kt

---
+++ src/main/kotlin/map/map_1.kt
@@ -0,0 +1,8 @@
+package map
+
+fun main () {
+    //declaring a mao of integer to string
+    val map = mapOf(1 to " Oybek", 2 to " Abduqodir", 3 to " Sonbe")
+    // printing the map
+    println(map)
+}
