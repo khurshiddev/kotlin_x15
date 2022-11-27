944d7bc3796ca10134725c8da95f4171cfcf917d
khurshiddev <umaribnxattob8008@gmail.com>
2022-11-27 23:14:24 +0900

    [add::] map_2.kt

---
+++ src/main/kotlin/map/map_2.kt
@@ -0,0 +1,9 @@
+package map
+
+fun main () {
+    val maps = mapOf(1 to "One", 2 to "Two", 3 to "Three", 4 to "Four")
+
+    println("Map Entries : "+maps)
+    println("Map Keys: "+maps.keys)
+    println("Map Values: "+maps.values)
+}
