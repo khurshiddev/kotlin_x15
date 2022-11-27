90970fb9e06c2eb44965634833ccbbbd14ec8516
khurshiddev <umaribnxattob8008@gmail.com>
2022-11-27 23:14:31 +0900

    [add::] map_3.kt

---
+++ src/main/kotlin/map/map_3.kt
@@ -0,0 +1,8 @@
+package map
+
+fun main () {
+    val map = mapOf(1 to "Uzbekistan", 2 to "Korea", 3 to "Canada", 4 to "America")
+
+    println("The size of the map is: "+map.size)
+    println("The size of the map is:"+map.count())
+}
