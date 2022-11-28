80d65a449c5f387a76a5c0ca9aa6b893970be0aa
khurshiddev <umaribnxattob8008@gmail.com>
2022-11-28 22:48:40 +0900

    [add::] map_8.kt

---
+++ src/main/kotlin/map/map_8.kt
@@ -0,0 +1,9 @@
+package map
+
+fun main () {
+    val theMap = mapOf("one" to 1, "two" to 2, "three" to 3, "four" to 4)
+    println(theMap)
+
+    val mutMap = mutableMapOf("one" to 1, "two" to 2, "three" to 3, "four" to 4)
+    println(mutMap)
+}
